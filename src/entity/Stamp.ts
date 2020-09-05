630b1b54334851c1118baafa4cb17307877aabf5
SELA <RiSEblackbird@users.noreply.github.com>
2020-09-05 13:21:12 +0900

    Columnモジュールは使用しないので削除

--- src/entity/Stamp.ts
+++ src/entity/Stamp.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from "typeorm";
 
 @Entity()
 export class Stamp {
