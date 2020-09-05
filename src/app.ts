7b463f02d9ac6ac23526311bc4733a40fbc81a4a
SELA <RiSEblackbird@users.noreply.github.com>
2020-09-05 13:20:00 +0900

    User -> Keyword に置換

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import {Request, Response} from "express";
 import {createConnection} from "typeorm";
 import {Keyword} from "./entity/Keyword";
 import {Stamp} from "./entity/Stamp";
-import {StudyLog} from "./entity/WordMemo";
+import {StudyLog} from "./entity/StudyLog";
 
 // create typeorm connection
 createConnection().then(connection => {
-    const userRepository = connection.getRepository(User);
+    const keywordRepository = connection.getRepository(Keyword);
 
     // create and setup express app
     const app = express();
@@ -15,31 +15,31 @@ createConnection().then(connection => {
 
     // register routes
 
-    app.get("/users", async function(req: Request, res: Response) {
-        const users = await userRepository.find();
-        res.json(users);
+    app.get("/keywords", async function(req: Request, res: Response) {
+        const keywords = await keywordRepository.find();
+        res.json(keywords);
     });
 
-    app.get("/users/:id", async function(req: Request, res: Response) {
-        const results = await userRepository.findOne(req.params.id);
+    app.get("/keywords/:id", async function(req: Request, res: Response) {
+        const results = await keywordRepository.findOne(req.params.id);
         return res.send(results);
     });
 
-    app.post("/users", async function(req: Request, res: Response) {
-        const user = await userRepository.create(req.body);
-        const results = await userRepository.save(user);
+    app.post("/keywords", async function(req: Request, res: Response) {
+        const keyword = await keywordRepository.create(req.body);
+        const results = await keywordRepository.save(keyword);
         return res.send(results);
     });
 
-    app.put("/users/:id", async function(req: Request, res: Response) {
-        const user = await userRepository.findOne(req.params.id);
-        userRepository.merge(user, req.body);
-        const results = await userRepository.save(user);
+    app.put("/keywords/:id", async function(req: Request, res: Response) {
+        const keyword = await keywordRepository.findOne(req.params.id);
+        keywordRepository.merge(keyword, req.body);
+        const results = await keywordRepository.save(keyword);
         return res.send(results);
     });
 
-    app.delete("/users/:id", async function(req: Request, res: Response) {
-        const results = await userRepository.delete(req.params.id);
+    app.delete("/keywords/:id", async function(req: Request, res: Response) {
+        const results = await keywordRepository.delete(req.params.id);
         return res.send(results);
     });
 
